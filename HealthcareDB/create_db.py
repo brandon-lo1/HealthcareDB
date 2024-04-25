1db5a2f494197936bc87230350756442dce89118
Brandon Lo <lobrandon667@gmail.com>
2024-04-25 14:35:20 -0400

    added appointments and patients page

--- HealthcareDB/create_db.py
+++ HealthcareDB/create_db.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from datetime import date, time
 
 def create_database(cursor):
     cursor.execute('CREATE DATABASE IF NOT EXISTS healthcare_db')
@@ -55,17 +56,20 @@ def create_tables(cursor):
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS lab_tests (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        test_type VARCHAR(50) NOT NULL),
+        test_type VARCHAR(50) NOT NULL,
         result VARCHAR(255) NOT NULL,
         date DATE NOT NULL)
         ''')
     
     # Insurance
     cursor.execute('''
-    CREATE TABLE IF NOT EXISTS insurance (
+    CREATE TABLE IF NOT EXISTS insurance_claims (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        
-        company VARCHAR(50) NOT NULL)
+        company VARCHAR(50) NOT NULL,
+        patient_id INT NOT NULL,
+        appointment_id INT NOT NULL,
+        FOREIGN KEY (patient_id) REFERENCES patients(id) ON UPDATE CASCADE ON DELETE CASCADE,
+        FOREIGN KEY (appointment_id) REFERENCES appointments(id) ON UPDATE CASCADE ON DELETE CASCADE)
         ''')
     
     # RELATIONSHIPS
@@ -77,7 +81,7 @@ def create_tables(cursor):
         med_id INT NOT NULL,
         PRIMARY KEY (doctor_id, patient_id, med_id),
         FOREIGN KEY (doctor_id) REFERENCES doctors(id) ON UPDATE CASCADE ON DELETE CASCADE,
-        FOREIGN KEY (patient_id) REFERENCES patients(id) ON UPDATE CASCADE ON DELETE CASCADE
+        FOREIGN KEY (patient_id) REFERENCES patients(id) ON UPDATE CASCADE ON DELETE CASCADE,
         FOREIGN KEY (med_id) REFERENCES medications(id) ON UPDATE CASCADE ON DELETE CASCADE)
         ''')
     
@@ -88,16 +92,22 @@ def create_tables(cursor):
         lab_id INT NOT NULL,
         PRIMARY KEY (doctor_id, patient_id, lab_id),
         FOREIGN KEY (doctor_id) REFERENCES doctors(id) ON UPDATE CASCADE ON DELETE CASCADE,
-        FOREIGN KEY (patient_id) REFERENCES patients(id) ON UPDATE CASCADE ON DELETE CASCADE
+        FOREIGN KEY (patient_id) REFERENCES patients(id) ON UPDATE CASCADE ON DELETE CASCADE,
         FOREIGN KEY (lab_id) REFERENCES lab_tests(id) ON UPDATE CASCADE ON DELETE CASCADE)
         ''')
     
-    # Insurance Claims
-
-
-
+    # MULTI-VALUED ATTRIBUTES
+    # Allergies
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS allergies (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        patient_id INT NOT NULL,
+        allergy VARCHAR(50) NOT NULL,
+        FOREIGN KEY (patient_id) REFERENCES patients(id) ON UPDATE CASCADE ON DELETE CASCADE)
+        ''')
+    
 def insert_sample_data(cursor):
-    # Start ID increment
+    # Start user ID increment
     id = 1
 
     # List of sample doctors
@@ -119,7 +129,7 @@ def insert_sample_data(cursor):
         ('patient1', 'patientpass1', 'Jane', 'Doe', 'R'),
         ('patient2', 'patientpass2', 'Jennie', 'Cannon', 'F'),
         ('patient3', 'patientpass3', 'Blanca', 'Blake', 'B'),
-        ('patient4', 'patientpass4', 'Brandon', 'Lo', 'S'),
+        ('patient4', 'patientpass4', 'Tory', 'Lanez', 'T'),
         ('patient5', 'patientpass5', 'Daryl', 'Sheldon', 'W'),
         ('patient6', 'patientpass6', 'Lola', 'Espinoza', 'A'),
         ('patient7', 'patientpass7', 'Dustin', 'Bryant', 'N'),
@@ -138,17 +148,135 @@ def insert_sample_data(cursor):
         ('patient20', 'patientpass20', 'Armando', 'Cooley', 'M'),
     ]
 
+    # List of sample appointments
+    appointments = [
+        (date(2024, 5, 7), time(12, 0), 1, 11),
+        (date(2024, 5, 4), time(10, 0), 1, 11),
+        (date(2024, 5, 24), time(13, 0), 7, 11),
+        (date(2024, 4, 27), time(10, 0), 5, 11),
+        (date(2024, 5, 23), time(16, 0), 5, 12),
+        (date(2024, 5, 6), time(8, 0), 5, 14),
+        (date(2024, 5, 14), time(14, 0), 9, 19),
+        (date(2024, 5, 4), time(10, 0), 8, 14),
+        (date(2024, 5, 18), time(8, 0), 1, 15),
+        (date(2024, 5, 22), time(12, 0), 2, 18),
+        (date(2024, 5, 23), time(9, 0), 5, 20),
+        (date(2024, 5, 9), time(11, 0), 1, 14),
+        (date(2024, 5, 13), time(8, 0), 2, 13),
+        (date(2024, 5, 18), time(13, 0), 6, 19),
+        (date(2024, 5, 20), time(9, 0), 5, 26),
+        (date(2024, 5, 7), time(12, 0), 10, 11),
+        (date(2024, 5, 2), time(8, 0), 6, 18),
+        (date(2024, 5, 25), time(16, 0), 7, 16),
+        (date(2024, 5, 6), time(8, 0), 4, 26),
+        (date(2024, 5, 11), time(12, 0), 6, 23),
+        (date(2024, 5, 17), time(8, 0), 8, 17),
+        (date(2024, 5, 19), time(13, 0), 3, 30),
+        (date(2024, 5, 20), time(8, 0), 10, 16),
+        (date(2024, 5, 8), time(15, 0), 10, 16),
+        (date(2024, 4, 30), time(13, 0), 7, 30),
+        (date(2024, 5, 6), time(15, 0), 8, 15),
+        (date(2024, 5, 10), time(16, 0), 9, 28),
+        (date(2024, 5, 5), time(11, 0), 1, 11),
+        (date(2024, 5, 9), time(11, 0), 4, 12),
+        (date(2024, 5, 16), time(9, 0), 8, 22),
+        (date(2024, 5, 22), time(13, 0), 10, 25),
+        (date(2024, 5, 21), time(14, 0), 6, 26),
+        (date(2024, 4, 25), time(8, 0), 9, 14),
+        (date(2024, 5, 2), time(8, 0), 9, 17),
+        (date(2024, 5, 17), time(14, 0), 7, 17),
+        (date(2024, 5, 13), time(12, 0), 8, 16),
+        (date(2024, 5, 16), time(16, 0), 2, 20),
+        (date(2024, 5, 17), time(14, 0), 10, 28),
+        (date(2024, 5, 7), time(13, 0), 2, 12),
+        (date(2024, 5, 13), time(12, 0), 2, 17),
+        (date(2024, 5, 12), time(15, 0), 8, 29),
+        (date(2024, 4, 29), time(10, 0), 2, 16),
+        (date(2024, 5, 10), time(10, 0), 4, 17),
+        (date(2024, 4, 29), time(13, 0), 5, 28),
+        (date(2024, 5, 3), time(14, 0), 5, 25),
+        (date(2024, 5, 10), time(15, 0), 9, 18),
+        (date(2024, 5, 18), time(8, 0), 4, 29),
+        (date(2024, 4, 25), time(9, 0), 9, 27),
+        (date(2024, 5, 15), time(14, 0), 3, 19),
+        (date(2024, 5, 6), time(8, 0), 8, 17)
+        ]
+    
+    # List of sample medications
+    medications = [
+        ('Vitamin D',),
+        ('Amoxicillin',),
+        ('Lisinopril',),
+        ('Ibuprofen',),
+        ('Prednisone',),
+        ('Alprazolam',),
+        ('Amphetamine',),
+        ('Atorvastatin',),
+        ('Amlopidine',),
+        ('Albuterol',)
+    ]
+
+    # List of sample lab tests
+    lab_tests = [
+        ('Blood Cholesterol', 'Normal', date(2024, 5, 8)),
+        ('Blood Glucose', 'High Blood Sugar', date(2024, 5, 10)),
+        ('Urinalysis', 'Normal', date(2024, 4, 29)),
+        ('Pap Smear', 'Normal', date(2024, 4, 28)),
+        ('Lipid Panel', 'Normal', date(2024, 5, 1))
+    ]
+    
+    # List of sample insurance claims
+    insurance = [
+        ('United Healthcare', 11, 1),
+        ('United Healthcare', 11, 2),
+        ('Anthem', 12, 3)
+    ]
+
+    # List of sample prescriptions
+    prescribed_meds = [
+        (1, 11, 3),
+        (8, 11, 4),
+        (5, 11, 7),
+        (1, 11, 9)
+    ]
+    prescribed_labs = [
+        (1, 11, 1),
+        (7, 11, 4),
+        (1, 11, 2),
+        (5, 11, 3),
+        (5, 12, 5)
+    ]
+
+    # List of sample allergies
+    allergies = [
+        (11, 'Nuts'),
+        (11, 'Dairy'),
+        (11, 'Shellfish'),
+        (12, 'Nuts'),
+        (12, 'Soy')
+    ]
+
+
     # Add doctors to users and doctors table and increment ID
     for doctor in doctors:
         cursor.execute('INSERT INTO users (id, username, password, first_name, last_name, middle_initial) VALUES (%s, %s, %s, %s, %s, %s)', (id, doctor[0], doctor[1], doctor[2], doctor[3], doctor[4]))
         cursor.execute('INSERT INTO doctors (id, specialty) VALUES (%s, %s)', (id, doctor[5]))
         id += 1
-        
+
     # Add patients to users and patients table and increment ID
     for patient in patients:
         cursor.execute('INSERT INTO users (id, username, password, first_name, last_name, middle_initial) VALUES (%s, %s, %s, %s, %s, %s)', (id, patient[0], patient[1], patient[2], patient[3], patient[4]))
         cursor.execute('INSERT INTO patients (id) VALUES (%s)', (id,))
         id += 1
+
+    # add all other sample data to tables
+    cursor.executemany("INSERT INTO appointments (date, time, doctor_id, patient_id) VALUES (%s, %s, %s, %s)", appointments)
+    cursor.executemany("INSERT INTO medications (medication_name) VALUES (%s)", medications)
+    cursor.executemany("INSERT INTO lab_tests (test_type, result, date) VALUES (%s, %s, %s)", lab_tests)
+    cursor.executemany("INSERT INTO insurance_claims (company, patient_id, appointment_id) VALUES (%s, %s, %s)", insurance)
+    cursor.executemany("INSERT INTO prescribed_med (doctor_id, patient_id, med_id) VALUES (%s, %s, %s)", prescribed_meds)
+    cursor.executemany("INSERT INTO prescribed_lab (doctor_id, patient_id, lab_id) VALUES (%s, %s, %s)", prescribed_labs)
+    cursor.executemany("INSERT INTO allergies (patient_id, allergy) VALUES (%s, %s)", allergies)
 
 
 def main():
@@ -156,17 +284,31 @@ def main():
     db = mysql.connector.connect(
         host='localhost',
         user='root',
-        passwd=''  # Replace with your own password
+        passwd='' # Replace with your own password
     )
     cursor = db.cursor()
 
     # Run functions to create database and tables, and then insert sample data
     try:
         create_database(cursor)
-        cursor.execute('DELETE FROM doctors')
-        cursor.execute('DELETE FROM patients')
-        cursor.execute('DELETE FROM users')
-        
+
+        # Uncomment this block if you need to delete/reset the rows in the database because of errors
+        # cursor.execute('DELETE FROM doctors')
+        # cursor.execute('DELETE FROM patients')
+        # cursor.execute('DELETE FROM users')
+        # cursor.execute('DELETE FROM appointments')
+        # cursor.execute('DELETE FROM medications')
+        # cursor.execute('DELETE FROM lab_tests')
+        # cursor.execute('DELETE FROM insurance_claims')
+        # cursor.execute('DELETE FROM prescribed_med')
+        # cursor.execute('DELETE FROM prescribed_lab')
+        # cursor.execute('DELETE FROM allergies')
+        # cursor.execute('ALTER TABLE appointments AUTO_INCREMENT = 1')
+        # cursor.execute('ALTER TABLE medications AUTO_INCREMENT = 1')
+        # cursor.execute('ALTER TABLE lab_tests AUTO_INCREMENT = 1')
+        # cursor.execute('ALTER TABLE insurance_claims AUTO_INCREMENT = 1')
+        # cursor.execute('ALTER TABLE allergies AUTO_INCREMENT = 1')
+
         create_tables(cursor)
         insert_sample_data(cursor)
         db.commit()
--- HealthcareDB/run.py
+++ HealthcareDB/run.py
@@ -19,6 +19,7 @@ def login():
         username = request.form['username']
         password = request.form['password']
         cursor = db.connection.cursor()
+        # Gets login and user information
         cursor.execute("SELECT id, username, password, first_name, last_name FROM users WHERE username = %s", (username,))
         user = cursor.fetchone()
         if user and user[2] == password:
@@ -34,7 +35,6 @@ def login():
                     role = 'patient'
                 else:
                     role = 'undefined'
-
             # Store session info
             session['logged_in'] = True
             session['user_id'] = user_id
@@ -48,12 +48,141 @@ def login():
 
 @app.route('/home')
 def home():
+    # Return different home pages for doctors and patients
     if session['role'] == 'doctor':
         return render_template('doctor_home.html', name = session.get('name'))
     elif session['role'] == 'patient':
         return render_template('patient_home.html', name = session.get('name'))
     return redirect(url_for('login'))
 
+@app.route('/appointments')
+def appointments():
+    if 'logged_in' in session and session['role'] == 'doctor':
+        cursor = db.connection.cursor()
+        # Find all future appointments
+        cursor.execute("""
+            SELECT u.first_name, u.last_name, a.date, a.time 
+            FROM appointments a
+            JOIN users u ON u.id = a.patient_id
+            WHERE a.doctor_id = %s AND a.date >= CURDATE()
+            ORDER BY a.date, a.time
+            """, (session['user_id'],))
+        future_appointments = cursor.fetchall()
 
+        # Find all past appointments
+        cursor.execute("""
+            SELECT u.first_name, u.last_name, a.date, a.time 
+            FROM appointments a
+            JOIN users u ON u.id = a.patient_id
+            WHERE a.doctor_id = %s AND a.date < CURDATE()
+            ORDER BY a.date, a.time
+            """, (session['user_id'],))
+        past_appointments = cursor.fetchall()
+        cursor.close()
+        return render_template('doctor_appointments.html', 
+            future_appointments = future_appointments, 
+            past_appointments = past_appointments, 
+            name = session.get('name'))
+    elif 'logged_in' in session and session['role'] == 'patient':
+        pass
+    else:
+        return redirect(url_for('login'))
+    
+@app.route('/patients')
+def patients():
+    if 'logged_in' in session and session['role'] == 'doctor':
+        cursor = db.connection.cursor()
+        # Find all distinct patients that the logged in doctor has an appointment with
+        cursor.execute("""
+            SELECT DISTINCT u.id, u.first_name, u.last_name
+            FROM users u
+            JOIN appointments a ON u.id = a.patient_id
+            WHERE a.doctor_id = %s
+            ORDER BY u.last_name, u.first_name
+            """, (session['user_id'],))
+        patients_data = cursor.fetchall()
+        cursor.close()
+        return render_template('doctor_patients.html', patients = patients_data)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/patient/<int:patient_id>')
+def patient_details(patient_id):
+    if 'logged_in' in session and session['role'] == 'doctor':
+        cursor = db.connection.cursor()
+        # Find patient name
+        cursor.execute('SELECT first_name, last_name FROM users WHERE id = %s', (patient_id,))
+        patient_name = cursor.fetchone()
+
+        # Find past appointments for this patient
+        cursor.execute("""
+            SELECT u.first_name, u.last_name, a.date, a.time 
+            FROM appointments a 
+            JOIN users u ON u.id = a.doctor_id
+            WHERE a.patient_id = %s AND a.date < CURDATE()
+            ORDER BY a.date, a.time
+            """, (patient_id,))
+        past_visits = cursor.fetchall()
+
+        # Find future appointments for this patient
+        cursor.execute("""
+            SELECT u.first_name, u.last_name, a.date, a.time 
+            FROM appointments a 
+            JOIN users u ON u.id = a.doctor_id
+            WHERE a.patient_id = %s AND a.date >= CURDATE()
+            ORDER BY a.date, a.time
+            """, (patient_id,))
+        future_visits = cursor.fetchall()
+
+        # Find all medications this patient is taking and doctor who prescribed
+        cursor.execute("""
+            SELECT m.medication_name, u.first_name, u.last_name
+            FROM medications m 
+            JOIN prescribed_med p ON p.med_id = m.id
+            JOIN users u ON u.id = p.doctor_id
+            WHERE p.patient_id = %s
+            """, (patient_id,))
+        medications = cursor.fetchall()
+
+        # Find all allergies for this patient
+        cursor.execute("""
+            SELECT a.allergy
+            FROM allergies a
+            WHERE a.patient_id = %s
+            """, (patient_id,))
+        allergies = cursor.fetchall()
+
+        # Find all lab tests for this patient
+        cursor.execute("""
+            SELECT l.test_type, l.result, u.first_name, u.last_name
+            FROM lab_tests l 
+            JOIN prescribed_lab p ON p.lab_id = l.id
+            JOIN users u ON u.id = p.doctor_id
+            WHERE p.patient_id = %s
+            """, (patient_id,))
+        labs = cursor.fetchall()
+
+        # Find all insurance claims for this patient
+        cursor.execute("""
+            SELECT i.company, a.date, u.first_name, u.last_name
+            FROM insurance_claims i
+            JOIN appointments a ON a.id = i.appointment_id
+            JOIN users u ON u.id = a.doctor_id
+            WHERE i.patient_id = %s
+            """, (patient_id,))
+        insurance = cursor.fetchall()
+        cursor.close()
+        return render_template('doctor_patientdetails.html', 
+            past_visits = past_visits, 
+            future_visits = future_visits,
+            medications = medications,
+            allergies = allergies,
+            labs = labs,
+            insurance = insurance,
+            patient_name = patient_name,
+            patient_id = patient_id)
+    else:
+        return redirect(url_for('login'))
+    
 if __name__ == '__main__':
     app.run(debug=True)
